34884bd2da3e3bc9c07a3b137dc20bca6804bd1b
JitenP <jitenp@outlook.com>
2023-02-27 11:20:17 +0530

    add funcs in map

--- 40-closure/main.go
+++ 40-closure/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 
-	c1 := arithmetic(10, 20, func(a, b int) int {
-		return a + b
+	c1 := arithmetic(10, 20, func(a1, b1 int) int {
+		return a1 + b1
 	})
 	fmt.Println("c1:", c1)
 
@@ -18,6 +18,30 @@ func main() {
 
 	c3 := arithmetic(10, 20, f3)
 	fmt.Println("c3:", c3)
+
+	var funcMap map[string]func(int, int) int
+
+	funcMap = make(map[string]func(int, int) int)
+
+	funcMap["add"] = func(a, b int) int {
+		return a + b
+	}
+
+	funcMap["sub"] = sub
+
+	funcMap["multiply"] = f3
+
+	a1 := funcMap["add"]
+
+	fmt.Println("addition=", a1(10, 20))
+
+	a2 := funcMap["sub"]
+
+	fmt.Println("substractions=", a2(10, 20))
+
+	a3 := funcMap["multiply"]
+
+	fmt.Println("multiplication=", a3(10, 20))
 
 }
 
