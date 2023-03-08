de4ad3c4c188fc71ee6839ab69852054a8ae9089
JitenP <jitenp@outlook.com>
2023-03-08 15:04:13 +0530

    add authenticate more functions

--- 62-contacts/main.go
+++ 62-contacts/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
+
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +22,17 @@ func main() {
 		// c.XML(http.StatusOK, m)
 	})
 
+	r.GET("/greet/:name", Authenticate(), func(c *gin.Context) {
+		if name, ok := c.Params.Get("name"); !ok {
+			c.String(http.StatusBadRequest, "name parameter has not been provided")
+			c.Abort()
+		} else {
+			c.String(http.StatusOK, "Hello "+name)
+		}
+		//c.Abort()
+		c.Next()
+	}, Audit)
+
 	// http.HandleFunc("/ping",func(w http.ResponseWriter, r *http.Request) {
 	// })
 	//http.ListenAndServe(":50090", nil)
@@ -33,6 +47,38 @@ func main() {
 		fmt.Println("This is being executed for the past-->", count*10, "seconds")
 	}
 
+}
+
+func Authenticate() func(*gin.Context) {
+	return func(c *gin.Context) {
+		email := c.Request.Header.Get("email")
+		password := c.Request.Header.Get("password")
+
+		if email == "" || password == "" {
+			c.String(http.StatusUnauthorized, "bad credentials")
+			c.Abort()
+		} else if email == "admin@questglobal.com" && password == "admin@123" {
+			c.Next()
+		} else {
+			c.String(http.StatusUnauthorized, "bad credentials")
+			c.Abort()
+		}
+	}
+}
+
+func Audit(c *gin.Context) {
+	url := c.Request.URL
+	file, err := os.OpenFile("audit.io", os.O_RDWR, 777)
+	if err != nil {
+		//fmt.Println(err)
+		log.Println(err)
+	}
+	defer file.Close()
+
+	_, err = file.Write([]byte(url.String()))
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 type Message struct {
@@ -51,4 +97,5 @@ type Message struct {
 // What this micro service is for
 // This does CRUD operations on contacts. Creating/ Reading/ Updating and Deleting contects in a database
 // Dependency injectin and also follow solid principles
-//
+
+// In order to pass the request , user has to pass valid email and password
