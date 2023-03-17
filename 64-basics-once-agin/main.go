9b4a3c20c8d77621b79a92439a759c20a7a79573
Jiten <jpalaparthi@gmail.com>
2023-03-17 16:25:39 +0530

    add goexit demo

---
+++ 64-basics-once-agin/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	go func() {
+		fmt.Println("I am running inside a go routine")
+		i := 1
+		for {
+			if i == 10 {
+				runtime.Goexit()
+			}
+			go fmt.Println("Hello World-->", i)
+			i++
+		}
+	}()
+	time.Sleep(time.Second * 1)
+}
