6591df245292801419f9287ad5db70619663626c
Jiten <jpalaparthi@gmail.com>
2023-03-20 11:32:40 +0530

    add one more type for dummy json

--- 66-dummyjson/main.go
+++ 66-dummyjson/main.go
@@ -13,14 +13,47 @@ func main() {
 		fmt.Println(err)
 	}
 
-	pd := new(PersonData)
+	pd1 := new(PersonData)
 
-	err = json.Unmarshal(bytes, pd)
+	err = json.Unmarshal(bytes, pd1)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(pd)
+	fmt.Println(pd1)
+
+	// another way
+	type PersonDetails2 = struct {
+		Id        string `json:"_id"`
+		Name      string `json:"name"`
+		DOB       string `json:"dob"`
+		Telephone string `json:"telephone"`
+		Email     string `json:"email"`
+		Url       string `json:"url"`
+
+		Score       float32 `json:"score"`
+		Description string  `json:"description"`
+		Verified    bool    `json:"verified"`
+		Salary      float32 `json:"salary"`
+
+		Address struct { // anonymous struct type
+			Street   string `json:"street"`
+			Town     string `json:"town"`
+			PostCode string `json:"postcode"`
+		} `json:"address"`
+
+		Pets []string `json:"pets"` // array
+	}
+
+	var pd2 PersonDetails2
+
+	err = json.Unmarshal(bytes, &pd2)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(pd2)
 }
 
 type PersonData struct {
@@ -36,11 +69,11 @@ type PersonData struct {
 	Verified    bool    `json:"verified"`
 	Salary      float32 `json:"salary"`
 
-	Address struct {
+	Address struct { // anonymous struct type
 		Street   string `json:"street"`
 		Town     string `json:"town"`
 		PostCode string `json:"postcode"`
 	} `json:"address"`
 
-	Pets []string `json:"pets"`
+	Pets []string `json:"pets"` // array
 }
