654cd85abec6f18306d8cb44609a0a18ce1702e1
Jiten <jpalaparthi@gmail.com>
2023-03-21 14:58:59 +0530

    add kafka consumer and producer

--- 62-contacts/mb/example/kafka-createtopic.go
+++ 62-contacts/mb/example/kafka-createtopic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	kafka "github.com/segmentio/kafka-go"
 )
@@ -14,4 +15,24 @@ func main() {
 	}
 
 	conn.Close()
+
+	conn, err = kafka.Dial("tcp", "localhost:29092")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	m := map[string]struct{}{}
+
+	for _, p := range partitions {
+		m[p.Topic] = struct{}{}
+	}
+	for k := range m {
+		fmt.Println(k)
+	}
 }
---
+++ mb-go-client/running-kafka.md
@@ -0,0 +1,8 @@
+- make sure that docker-compose is installed
+
+```docker-compose version```
+
+- run kafka docker-compose file which is in dockers directory
+
+```docker compose -f kafka-compose.yaml up -d```
+
