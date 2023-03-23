74e13fabfa86a1d94f7014d78bc25d16fa886af0
Jiten <jpalaparthi@gmail.com>
2023-03-23 17:51:39 +0530

    add docker commands in docker file

--- docker-training/helloworld/main.go
+++ docker-training/helloworld/main.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	port string
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("server started and working")
+
+	port = os.Getenv("PORT")
+
+	if port == "" {
+		flag.StringVar(&port, "port", "8080", "--port=8080")
+		flag.Parse()
+	}
+
+	count := 0
+	go func() {
+		for {
+			count++
+			time.Sleep(time.Second * 10)
+			fmt.Println("The server is running from the past ", count*10, "seconds")
+		}
+	}()
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "pong")
+	})
+
+	http.ListenAndServe(":"+port, nil)
 }
 
 // push this code to docker.
---
+++ tasks/task-24th-march-2023.md
@@ -0,0 +1,16 @@
+- Create a restful service for Employee(Id,Name,Address,Email,Status,LastModified) Using gin
+
+- Create a Dockerfile for the service
+
+- The service must have two environment variables 
+
+- PORT : Port of the service
+- DSN: Database connection 
+- Both must can be passed as env as well as command line args
+
+- Start the postgres container.
+
+- build and run the docker container of the service.
+
+- Container must automatically connect to the database and work.
+
